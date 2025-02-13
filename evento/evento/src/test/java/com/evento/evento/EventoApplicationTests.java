609b915636bd9dbcecbea1b784fc882465e4d383
xyms6 <luis.surf2000@gmail.com>
2025-02-13 16:02:00 -0300

    Add files via upload

---
+++ evento/evento/src/test/java/com/evento/evento/EventoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evento.evento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
