9487187513525209bbb205bb5ca882e89c937131
xyms6 <luis.surf2000@gmail.com>
2025-02-13 15:57:15 -0300

    teste

---
+++ com/evento/evento/repository/AtividadeRepository.java
@@ -0,0 +1,9 @@
+package com.evento.evento.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.evento.evento.entidade.Atividade;
+
+public interface AtividadeRepository extends JpaRepository<Atividade, Integer> {
+
+}
